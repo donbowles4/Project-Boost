b96683e3f1c3c015faa3806e03caf6558f0653d3
Donald Bowles <donniebowles84@gmail.com>
2022-09-12 13:30:07 -0400

    Basic Input Binding

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,6 +13,25 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("THRUSTING");
+        }
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotate Right");
+        }
     }
 }

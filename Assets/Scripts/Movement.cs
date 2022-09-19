dc4a8d941a6df89f4e92a7de81e2692d962f93c5
Donald Bowles <donniebowles84@gmail.com>
2022-09-19 13:02:15 -0400

    Play Audio Source

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    AudioSource audioSource;
     [SerializeField] float fltRotThrust = 1;
     [SerializeField] float fltMainThrust = 1000;
     Rigidbody rb;
@@ -11,6 +12,7 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -23,7 +25,12 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * fltMainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else { audioSource.Stop(); }
     }
     void ProcessRotation()
     {

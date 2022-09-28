0e6dfb4394f6eaff1df1de4aee528072a5cd811f
Donald Bowles <donniebowles84@gmail.com>
2022-09-28 12:42:26 -0400

    mathf.sin

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -7,6 +7,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
     [SerializeField] [Range(0,1)] float fltMovementFactor;
+    [SerializeField] float fltPeriod = 2f;
     void Start()
     {
         startingPosition = transform.position;
@@ -14,6 +15,14 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        if(fltPeriod == 0) { return; }
+        float fltCycles = Time.time / fltPeriod; //always growing
+
+        const float fltTau = Mathf.PI * 2; //constant value 6.283
+        float fltRawSinWave = Mathf.Sin(fltCycles * fltTau);
+
+        fltMovementFactor = (fltRawSinWave + 1f) / 2f;
+
         Vector3 offset = movementVector * fltMovementFactor;
         transform.position = startingPosition + offset;
     }
